44c27b812cf5a94ab6c6b0c6702573ae7f0a3b1c
m4r1oph <94112810+m4r1oph@users.noreply.github.com>
2022-06-01 17:22:06 +0200

    Update Synology-vmm-backup.sh

--- Synology-vmm-backup.sh
+++ Synology-vmm-backup.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-synowebapi --exec api=SYNO.Virtualization.API.Guest.Action version=1 method=shutdown runner=admin guest_name="Windows2016"
+synowebapi --exec api=SYNO.Virtualization.API.Guest.Action version=1 method=shutdown runner=admin guest_name="NAME_OF_VM"
 sleep 15m
 
-/var/packages/Virtualization/target/bin/vmm_backup_ova --dst=Backup --batch=1 --guests="Windows2016"
-num_backups=$(find /volume1/Backup/Windows2016 -maxdepth 1 -type f | wc -l)
+/var/packages/Virtualization/target/bin/vmm_backup_ova --dst=Backup --batch=1 --guests="NAME_OF_VM"
+num_backups=$(find /ROUTE/TO/THE/DESTINATION/FOLDER/ -maxdepth 1 -type f | wc -l)
 if [[ $num_backups -gt 3 ]]
     then
-        mkdir /volume1/Backup/Windows2016/logs
-        backup_old=$(find /volume1/Backup/Windows2016 -maxdepth 1 -type f -printf '%T+ %p\n' | sort > /volume1/Backup/Windows2016/logs/fich.txt)
-        num_lin=$(cat /volume1/Backup/Windows2016/logs/fich.txt | wc -l )
+        mkdir /ROUTE/TO/THE/DESTINATION/FOLDER/logs
+        backup_old=$(find /ROUTE/TO/THE/DESTINATION/FOLDER/ -maxdepth 1 -type f -printf '%T+ %p\n' | sort > /ROUTE/TO/THE/DESTINATION/FOLDER/logs/fich.txt)
+        num_lin=$(cat /ROUTE/TO/THE/DESTINATION/FOLDER/logs/fich.txt | wc -l )
         num_old=$(($num_lin-3))
         if [[ $num_old -gt 0 ]]
             then
                 num_delete=$(($num_old+1))
-                cat /volume1/Backup/Windows2016/logs/fich.txt | head -n $num_old | cut -d " " -f 2 > /volume1/Backup/Windows2016/logs/del_old.txt
+                cat /ROUTE/TO/THE/DESTINATION/FOLDER/logs/fich.txt | head -n $num_old | cut -d " " -f 2 > /ROUTE/TO/THE/DESTINATION/FOLDER/logs/del_old.txt
                 num_lin=$(cat /volume1/Backup/Windows2016/logs/del_old.txt | wc -l )
                 for i in $(seq 1 $num_lin)
                     do
-                        delete=$(cat /volume1/Backup/Windows2016/logs/del_old.txt | sed -n $i'p')
+                        delete=$(cat /ROUTE/TO/THE/DESTINATION/FOLDER/logs/del_old.txt | sed -n $i'p')
                         rm $delete
                     done
             else
-                backup_old=$(find /volume1/Backup/Windows2016 -maxdepth 1 -type f -printf '%T+ %p\n' | sort | head -n 1 | cut -d " " -f 2)
+                backup_old=$(find /ROUTE/TO/THE/DESTINATION/FOLDER/ -maxdepth 1 -type f -printf '%T+ %p\n' | sort | head -n 1 | cut -d " " -f 2)
                 rm $backup_old
             fi
-            rm -r /volume1/Backup/Windows2016/logs
+            rm -r /ROUTE/TO/THE/DESTINATION/FOLDER/logs
 fi
 
-hyper_backup=$(find /volume1/Backup/Windows2016 -maxdepth 1 -type f -printf '%T+ %p\n' | sort | tail -n 1 | cut -d " " -f 2)
+hyper_backup=$(find /ROUTE/TO/THE/DESTINATION/FOLDER/ -maxdepth 1 -type f -printf '%T+ %p\n' | sort | tail -n 1 | cut -d " " -f 2)
 cp $hyper_backup /volume1/Backup/Windows2016/Hyper_Backup/WS2016.ova
 
-synowebapi --exec api=SYNO.Virtualization.API.Guest.Action version=1 method=poweron runner=admin guest_name="Windows2016"
+synowebapi --exec api=SYNO.Virtualization.API.Guest.Action version=1 method=poweron runner=admin guest_name="NAME_OF_VM"
